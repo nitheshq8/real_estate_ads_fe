ae2614d9bc45c06b0b31f0a758be1672062afb5c
nitheshq8 <nithesh@q8coders.com>
2025-03-06 22:27:48 +0530

    build fix 9

--- next.config.mjs
+++ next.config.mjs
@@ -141,7 +141,7 @@ export default withPWA({
   dest: "public",
   register: true,
   skipWaiting: true,
-  disable: process.env.NODE_ENV === "development",
+//   disable: process.env.NODE_ENV === "development",
   fallbacks: {
     document: "/offline",
   },
